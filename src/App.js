e14c78eb55c3b2b4d710bea55cd1e7300b295cf3
Estrela Espiridião <93261144+EstrelaE@users.noreply.github.com>
2022-09-02 14:35:11 +0100

    create search engine component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import logo from "./logo.png";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Dictionary App</h1>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App.logo img-fluid" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Estrela Espiridião</footer>
+      </div>
     </div>
   );
 }
