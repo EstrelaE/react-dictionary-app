874a89ee6f4638aee6bdc24514a1532d7090ac2f
EstrelaE <estrela.palma@hotmail.com>
2022-09-07 22:06:17 +0100

    try to debbug deploy error

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -4,9 +4,7 @@ export default function Phonetics(props) {
   console.log(props.data);
   return (
     <div className="Phonetics">
-      <a href={props.data.audio} target="_blank">
-        Listen
-      </a>
+      <a href={props.data.audio}>Listen</a>
       <br />
       {props.data.text}
     </div>
