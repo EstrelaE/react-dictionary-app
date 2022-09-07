fbcb03359e9c0a6335bfbcd66c05fb7b6293bb08
EstrelaE <estrela.palma@hotmail.com>
2022-09-07 22:17:14 +0100

    try to fix deploy bug

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -4,7 +4,8 @@ export default function Phonetics(props) {
   console.log(props.data);
   return (
     <div className="Phonetics">
-    
+      <a href={props.data.audio}>Listen</a>
+      <br />
       {props.data.text}
     </div>
   );
