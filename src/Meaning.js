ac27879362323d206055a003bc62d6f85a9ffae3
EstrelaE <estrela.palma@hotmail.com>
2022-09-07 18:32:36 +0100

    reduce results displayed

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,17 +3,33 @@ import React from "react";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
+      <h3 className="mt-4">{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map((definition, index) => {
-        return (
-          <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              <em>{definition.example}</em>
-            </p>
-          </div>
-        );
+        if (index < 1) {
+          return (
+            <div key={index}>
+              <h5>meaning:</h5>
+              <p>
+                {definition.definition}
+                <br />
+                <em>{definition.example}</em>
+              </p>
+            </div>
+          );
+        } else if (index < 4) {
+          return (
+            <div key={index}>
+              <h6>also:</h6>
+              <p>
+                {definition.definition}
+                <br />
+                <em>{definition.example}</em>
+              </p>
+            </div>
+          );
+        } else {
+          return null;
+        }
       })}
     </div>
   );
